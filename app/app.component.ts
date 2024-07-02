8227c47803332dfe6acf4b64fb6238b639fcab72
AlperJoe <91626308+AlperJoe@users.noreply.github.com>
2024-07-02 08:53:36 +0300

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {Post} from "./posts/post.model"
+
+// // Define the Post interface
+// interface Post {
+//   title: string;
+//   content: string;
+// }
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+}
